7f739ab9bf6a72691ef2133ea018ad56989e68e7
Clemens Hemmerling <clemens@getkor.io>
2021-08-11 12:40:12 -0600

    fix recaptcha

--- src/Recaptcha.js
+++ src/Recaptcha.js
@@ -1,19 +1,13 @@
-import React, { useEffect, useCallback }  from 'react';
+import React, { useEffect, useState }  from 'react';
 import axios from 'axios';
 import {
-  useGoogleReCaptcha
+  GoogleReCaptcha
 } from 'react-google-recaptcha-v3';
 
 const Recaptcha = () => {
-  const { executeRecaptcha } = useGoogleReCaptcha();
-
+  const [token, setToken] = useState('');
   // Create an event handler so you can call the verification on button click event or form submit
-  const handleRecaptchaVerify = useCallback(async () => {
-    if (!executeRecaptcha) {
-      console.log('Execute recaptcha not yet available');
-    }
-
-    const token = await executeRecaptcha('submit');
+  const handleGet = ()  => {
     // Do whatever you want with the token
     console.log(token);
     axios.get('SECURE_API_URL', { headers: { token } })
@@ -23,16 +17,22 @@ const Recaptcha = () => {
     .catch(error => {
       console.log(error)
     })
-  }, []);
+  };
 
   // You can use useEffect to trigger the verification as soon as the component being loaded
   useEffect(() => {
-    handleRecaptchaVerify();
-    const el = document.querySelector('.grecaptcha-badge');
-    el.style.display = 'none';
-  }, [handleRecaptchaVerify]);
+    handleGet();
+  }, []);
 
-  return <button id="submit" onClick={handleRecaptchaVerify}>Verify recaptcha</button>;
+  return (
+    <>
+      <GoogleReCaptcha
+        onVerify={token => {
+          setToken(token);
+        }}
+      />
+    </>
+  );
 };
 
 export default Recaptcha;
